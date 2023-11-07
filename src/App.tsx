28806d37e2bf40f2f5469f153f1bd04b1c49f45e
Sam Ibraimoff <sam.ibraimoff@gmail.com>
2023-11-07 00:03:02 -0500

    add themes, context and useTheme hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,23 @@
 import { Route, Routes } from "react-router-dom"
-import './index.scss'
+import './styles/index.scss'
 import { Link } from "react-router-dom"
 import { AboutPage } from "./pages/about-page/about-page-lazy"
 import { MainPage } from "./pages/main-page/main-page-lazy"
+import { useContext, useState } from "react"
+import { Theme, ThemeContext } from "./theme/theme-context"
+import { useTheme } from "./theme/useTheme"
 
 export const App = () => {
-    return <div className="app">
+  const {theme, toggleTheme} = useTheme()
+    return <div className={`app ${theme}`}>
         <nav>
             <Link to='/about'>about</Link>
             <Link to='/'>main</Link>
+            <button onClick={toggleTheme}>theme</button>
         </nav>
-        
-        <Routes>
-            <Route path="/" element={<MainPage />} />
-            <Route path="/about" element={<AboutPage />} />
-        </Routes>
+            <Routes>
+                <Route path="/" element={<MainPage />} />
+                <Route path="/about" element={<AboutPage />} />
+            </Routes>
     </div>
 }
--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,12 @@ import {render} from 'react-dom'
 import { App } from './App'
 import {BrowserRouter} from 'react-router-dom'
 import { Suspense } from "react"
+import { ThemeProvider } from './theme/theme-provider'
 
 render(<BrowserRouter>
     <Suspense fallback='loading'>
-        <App />
+        <ThemeProvider>
+            <App />
+        </ThemeProvider>
     </Suspense>
 </BrowserRouter>, document.getElementById('root'))
