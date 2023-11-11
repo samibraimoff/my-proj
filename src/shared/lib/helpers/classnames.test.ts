894f32f38f0c13f516b7cc219443bacffa15d70e
Sam Ibraimoff <sam.ibraimoff@gmail.com>
2023-11-11 01:56:44 -0500

    add jest and write first unit test

---
+++ src/shared/lib/helpers/classnames.test.ts
@@ -0,0 +1,22 @@
+import { classnames } from './classnames'
+
+describe('classnames', () => {
+  test('with only argument', () => {
+    expect(classnames('className', {})).toBe('className')
+  })
+
+  test('with additional argument', () => {
+    const expected = 'className class1 class2'
+    expect(classnames('className', {}, ['class1', 'class2'])).toBe(expected)
+  })
+
+  test('with Mods and additional argument', () => {
+    const expected = 'className class1 class2 hovered'
+    expect(classnames('className', { hovered: true }, ['class1', 'class2'])).toBe(expected)
+  })
+
+  test('with Mods (1 true and 1 false and 1 undefined) and additional argument', () => {
+    const expected = 'className class1 class2 hovered'
+    expect(classnames('className', { hovered: true, scrollable: false }, ['class1', 'class2'])).toBe(expected)
+  })
+})
